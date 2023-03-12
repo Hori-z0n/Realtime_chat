e247216ba253813e410601173c8801485e05066b
Konlawat Chuabswasdi <6410301016@cdti.ac.th>
2023-03-12 21:57:30 +0700

    comment

--- config.inc.php
+++ config.inc.php
@@ -6,6 +6,6 @@ $password = "at89mega328";
 $db = "webboard";
 $condb = mysqli_connect($host ,$user ,$password ,$db);
 ("connect error : ". mysqli_connect_error());
-echo 'connect to database complete';
+//echo 'connect to database complete';
 mysqli_set_charset($condb, 'utf8');
 ?>
